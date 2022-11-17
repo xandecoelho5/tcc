7185f2758a63f87229ed71fff2a93dfca17a9938
xandecoelho5 <xandecoelho5@hotmail.com>
2022-11-17 12:57:34 -0300

    added bean for authFilter

--- e_markety/src/main/java/br/edu/utfpr/e_markety/config/BeanConfiguration.java
+++ e_markety/src/main/java/br/edu/utfpr/e_markety/config/BeanConfiguration.java
@@ -1,13 +1,32 @@
 package br.edu.utfpr.e_markety.config;
 
+import br.edu.utfpr.e_markety.config.security.AuthByTokenFilter;
+import br.edu.utfpr.e_markety.config.security.service.TokenService;
+import br.edu.utfpr.e_markety.repository.EmpresaRepository;
+import br.edu.utfpr.e_markety.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.servlet.Filter;
+
 @Configuration
+@EnableJpaAuditing
+@RequiredArgsConstructor
 public class BeanConfiguration {
+
+    private final UsuarioRepository usuarioRepository;
+    private final TokenService tokenService;
+    private final EmpresaRepository empresaRepository;
+
+    @Bean
+    public Filter authByTokenFilter() {
+        return new AuthByTokenFilter(tokenService, usuarioRepository, empresaRepository);
+    }
 
     @Bean
     public ModelMapper getModelMapper() {
--- e_markety/src/main/java/br/edu/utfpr/e_markety/config/security/SecurityConfiguration.java
+++ e_markety/src/main/java/br/edu/utfpr/e_markety/config/security/SecurityConfiguration.java
@@ -1,13 +1,9 @@
 package br.edu.utfpr.e_markety.config.security;
 
-import br.edu.utfpr.e_markety.config.security.service.TokenService;
 import br.edu.utfpr.e_markety.config.security.service.UsuarioService;
-import br.edu.utfpr.e_markety.repository.EmpresaRepository;
-import br.edu.utfpr.e_markety.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -18,44 +14,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.Filter;
+
 import static br.edu.utfpr.e_markety.config.security.ConfigUtils.*;
 
 @EnableWebSecurity
 @Configuration
-@EnableJpaAuditing
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-    private final UsuarioRepository usuarioRepository;
+    private final Filter authByTokenFilter;
     private final UsuarioService usuarioService;
-    private final TokenService tokenService;
-    private final EmpresaRepository empresaRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .passwordManagement(management -> management.changePasswordPage("/update-password"))
-                .authorizeHttpRequests(
-                        authorize -> {
-                            try {
-                                authorize
-                                        .antMatchers(HttpMethod.GET, GET_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
-                                        .antMatchers(HttpMethod.POST, POST_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
-                                        .antMatchers(HttpMethod.PUT, PUT_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
-                                        .antMatchers(HttpMethod.DELETE, DELETE_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
-                                        .antMatchers(HttpMethod.PATCH, PATCH_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
-                                        .antMatchers(HttpMethod.GET, "/empresa", "/usuario/current").permitAll()
-                                        .antMatchers(HttpMethod.POST, "/auth/**", "/usuario").permitAll()
-                                        .anyRequest().authenticated()
-                                        .and().csrf().disable()
-                                        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                                        .and().addFilterBefore(new AuthByTokenFilter(tokenService, usuarioRepository, empresaRepository), UsernamePasswordAuthenticationFilter.class);
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                );
+            .authorizeHttpRequests(
+                authorize -> {
+                    try {
+                        authorize
+                            .antMatchers(HttpMethod.GET, GET_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
+                            .antMatchers(HttpMethod.POST, POST_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
+                            .antMatchers(HttpMethod.PUT, PUT_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
+                            .antMatchers(HttpMethod.DELETE, DELETE_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
+                            .antMatchers(HttpMethod.PATCH, PATCH_URLS).hasAnyRole("ADMIN", "FUNCIONARIO")
+                            .antMatchers(HttpMethod.GET, "/empresa", "/usuario/current").permitAll()
+                            .antMatchers(HttpMethod.POST, "/auth/**", "/usuario").permitAll()
+                            .anyRequest().authenticated()
+                            .and().csrf().disable()
+                            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                            .and().addFilterBefore(authByTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            );
         return http.build();
     }
 
